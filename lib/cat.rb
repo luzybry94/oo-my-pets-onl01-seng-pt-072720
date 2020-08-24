a55a794a68ad9f0cbe6ca1dfcb8e2ee24dc8585e
Chaim Bryski <luzybry94@gmail.com>
2020-08-24 20:16:04 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,11 +1,12 @@
 class Cat
   # code goes here
   attr_accessor :owner
-  attr_reader :name
+  attr_reader :name, :mood
   
   def initialize(name, owner)
     @name = name
     @owner = owner
+    @mood = "nervous"
   end
   
 end
